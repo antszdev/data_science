4f96633758cc837d27a6cc9f2dafeced0c250cf0
Aneta <anetas.dev@gmail.com>
2022-06-23 22:41:23 +0200

    nauka sql

---
+++ sql/nauka-pierwsza-lekcja.sql
@@ -0,0 +1,23 @@
+/*
+select
+from
+order by
+group by
+having
+where
+
+* - oznacza wszystkie kolumny
+
+Podstawowa struktura zapytania:
+select [lista kolumn] [skad] [opcjonalnie: sortowanie]
+*/
+
+/*
+wiecej niz jedna linia
+*/
+-- jedna linia
+
+--select * from Employees
+
+select top 3 * from Employees
+
